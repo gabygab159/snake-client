1f90c30e1af9cca8b78e271ff7c94588acea8a02
Gabriel <gabriel.jchen@gmail.com>
2021-03-10 18:20:13 +0000

    refactored

---
+++ client.js
@@ -0,0 +1,16 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: "135.23.222.131",
+    port: 50542
+  });
+  conn.setEncoding('utf8');
+  conn.on("data", (data) => {
+    console.log("data came back from server...");
+    console.log(data);
+  })
+  return conn;
+}
+
+module.exports = connect;
