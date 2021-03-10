87da72b57c8aa52b72edf2d9ae1e78548570912d
Gabriel <gabriel.jchen@gmail.com>
2021-03-10 18:13:03 +0000

    created client side thing

---
+++ play.js
@@ -0,0 +1,19 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: "135.23.222.131",
+    port: 50542
+  });
+  conn.setEncoding('utf8');
+  conn.on("data", (data) => {
+    console.log("data came back from server...");
+    console.log(data);
+  })
+  return conn;
+}
+
+console.log('Connecting ...');
+
+connect();
+
