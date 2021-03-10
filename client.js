c076536d9da941d77c15cad60e9790bfde936937
Gabriel <gabriel.jchen@gmail.com>
2021-03-10 19:20:29 +0000

    tried move commands

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require('net');
+// const { compileFunction } = require('node:vm');
 
 const connect = function() {
   const conn = net.createConnection({
@@ -10,12 +11,19 @@ const connect = function() {
     console.log("data came back from server: ");
     console.log(data);
   })
-  conn.write('Name: Gab')
+  conn.write('Name: Gab') 
+
   conn.on("connect", (connect) => {
     console.log("Successfully connected to game server");
     console.log(connect)
+      
+    conn.write("Move: up",(connect) => {
+      console.log("you moved up!")
+      console.log(connect)
+    })
   })
   return conn;
 }
+
 
 module.exports = connect;
